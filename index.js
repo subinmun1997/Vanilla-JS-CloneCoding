7059d0af0c77f97e1f00b7af6d03c928ebf9b582
subinmun1997 <qzxy812@gmail.com>
2020-10-07 18:04:13 +0900

    clone_coding

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Im Working. Im JS. Im Beautiful. Im worth it');
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "rgb(52,73,94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick(){
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    }else{
+        title.style.color = BASE_COLOR;
+    }
+}
+
+function init(){
+    title.style.color=BASE_COLOR;
+    title.addEventListener("mouseenter",handleClick);
+}
+init();
